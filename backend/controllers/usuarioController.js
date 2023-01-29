8931871842b4f291356d6fe76487fe5ee9b02490
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-01-28 21:58:41 -0300

    criação da função de autenticação de login e controller de autenticação

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,4 +1,5 @@
 const models = require('../models')
+const jwt = require('jsonwebtoken')
 
 //controller de usuario
 
@@ -7,8 +8,48 @@ const usuarioController = {
     res.send('Cadastro')
   },
 
-  login: async (req, res) => {
-    res.send('Login')
+  loginCandidato: async (req, res) => {
+    const candidato = models.Curriculo
+
+    let usuario = await candidato.findOne({
+      where: {
+        email: req.body.email,
+        senha: req.body.senha
+      }
+    })
+
+    if (!usuario)
+      res.status(400).send({ erro: true, message: 'email ou senha inválido' })
+
+    const token = jwt.sign(
+      { _id: usuario._id },
+      process.env.TOKEN_SECRET_CANDIDATO
+    )
+
+    res.header('authorization-token', token)
+    res.send('Logado')
+  },
+
+  loginEmpresa: async (req, res) => {
+    const empresa = models.Empresa
+
+    let usuario = await empresa.findOne({
+      where: {
+        email: req.body.email,
+        senha: req.body.senha
+      }
+    })
+
+    if (!usuario)
+      res.status(400).send({ erro: true, message: 'email ou senha inválido' })
+
+    const token = jwt.sign(
+      { _id: usuario._id },
+      process.env.TOKEN_SECRET_EMPRESA
+    )
+
+    res.header('authorization-token', token)
+    res.send('Logado')
   }
 }
 
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const usuarioController = require('../controllers/usuarioController')
+const authCandidato = require('../controllers/authCandidatoController') //controller de autenticação de candidato
+const authEmpresa = require('../controllers/authEmpresaController') //controller de autenticação de empresa
 
 //arquivo para colocar as rotas
 
@@ -8,6 +10,13 @@ const usuarioController = require('../controllers/usuarioController')
 router.post('/cadastro', usuarioController.cadastro)
 
 //rota de login
-router.post('/login', usuarioController.login)
+router.post('/login/candidato', usuarioController.loginCandidato)
+
+router.post('/login/empresa', usuarioController.loginEmpresa)
+
+//Exemplo para usar alguns dos controllers de autorização
+// router.get('/vagas', authEmpresa, (res) => {
+//   res.send('Somente usuário empresa pode acessar está rota')
+// })
 
 module.exports = router
