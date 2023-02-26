0abaa907e341b5a22102f3afca29cce7033c7ade
Maria Fernanda <mafe.almeida@eurekalabs.com.br>
2023-02-26 14:41:47 -0300

    feat: add states of curriculoStore

--- frontend/src/store/curriculoStore.ts
+++ frontend/src/store/curriculoStore.ts
@@ -1,10 +1,26 @@
 import { makeAutoObservable } from 'mobx';
-
+import { ItensList } from '../types/curriculo';
 export interface CurriculoStoreType {
   nomeEmpresa: string;
   setNomeEmpresa: (
     nomeEmpresa: string
   ) => void | React.Dispatch<React.SetStateAction<string>>;
+
+  inicio: string;
+  setInicio: (
+    inicio: string
+  ) => void | React.Dispatch<React.SetStateAction<string>>;
+
+  fim: string;
+  setFim: (fim: string) => void | React.Dispatch<React.SetStateAction<string>>;
+
+  cargo: string;
+  setCargo: (
+    fim: string
+  ) => void | React.Dispatch<React.SetStateAction<string>>;
+
+  experiencias: Array<ItensList>;
+  setExperiencias: (experiencias: Array<ItensList>) => void;
 }
 
 export class CurriculoStore implements CurriculoStoreType {
@@ -15,5 +31,25 @@ export class CurriculoStore implements CurriculoStoreType {
   nomeEmpresa: string = '';
   setNomeEmpresa(nomeEmpresa: string) {
     this.nomeEmpresa = nomeEmpresa;
+  }
+
+  inicio: string = '';
+  setInicio(inicio: string) {
+    this.inicio = inicio;
+  }
+
+  fim: string = '';
+  setFim(fim: string) {
+    this.fim = fim;
+  }
+
+  cargo: string = '';
+  setCargo(cargo: string) {
+    this.cargo = cargo;
+  }
+
+  experiencias: Array<ItensList> = [];
+  setExperiencias(experiencias: Array<ItensList>) {
+    this.experiencias = experiencias;
   }
 }
