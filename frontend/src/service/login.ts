68452330127c799441b2fe3a048707c9a7683106
Maria Fernanda <mafe.almeida@eurekalabs.com.br>
2023-03-12 12:02:31 -0300

    feat: merge conflicts with main and new login store

--- frontend/src/service/login.ts
+++ frontend/src/service/login.ts
@@ -2,34 +2,19 @@ import { FormEvent } from 'react';
 import { api } from '../lib/axios';
 
 export const autenticacaoLoginCandidato = async (
-  e: FormEvent,
   email: string,
-  senha: string,
-  setError: (error: string) => void,
-  setCanNavigate: (canNavigat: boolean) => void
+  senha: string
 ) => {
-  e.preventDefault();
-
   if (email === '' || senha === '') {
     return;
   }
 
-  await api
-    .post('usuario/login/candidato', {
-      email,
-      senha,
-    })
-    .then((res) => {
-      if (res.data.erro) {
-        setError(res.data.mensagem);
-        console.log('ENTROU AQUI 1');
-      } else {
-        console.log('ENTROU AQUI');
-        localStorage.setItem('id', res.data.id);
-        localStorage.setItem('token', res.data.token);
-        setCanNavigate(true);
-      }
-    });
+  const response = await api.post('usuario/login/candidato', {
+    email,
+    senha,
+  });
+
+  return response;
 };
 
 export const autenticacaoLoginEmpresa = async (
--- frontend/src/store/loginStore.ts
+++ frontend/src/store/loginStore.ts
@@ -1,8 +1,20 @@
 import { makeAutoObservable } from 'mobx';
+import { autenticacaoLoginCandidato } from '../service/login';
 
 export interface LoginStoreType {
   typeUser: string;
   setTypeUser: (typeUser: string) => void;
+  email: string;
+  setEmail: (email: string) => void;
+  senha: string;
+  setSenha: (senha: string) => void;
+  authCandidato: () => void;
+  error: string;
+  setError: (error: string) => void;
+  token: string;
+  setToken: (token: string) => void;
+  idUser: string;
+  setIdUser: (idUser: string) => void;
 }
 
 export class LoginStore implements LoginStoreType {
@@ -13,5 +25,46 @@ export class LoginStore implements LoginStoreType {
   typeUser: string = '';
   setTypeUser = (typeUser: string) => {
     this.typeUser = typeUser;
+  };
+
+  email: string = '';
+  setEmail = (email: string) => {
+    this.email = email;
+  };
+
+  senha: string = '';
+  setSenha = (senha: string) => {
+    this.senha = senha;
+  };
+
+  error: string = '';
+  setError = (error: string) => {
+    this.error = error;
+  };
+
+  token: string = '';
+  setToken = (token: string) => {
+    this.token = token;
+  };
+
+  idUser: string = '';
+  setIdUser = (idUser: string) => {
+    this.idUser = idUser;
+  };
+
+  authCandidato = async () => {
+    const response = await autenticacaoLoginCandidato(this.email, this.senha);
+
+    if (response?.data.erro) {
+      this.setError(response.data.mensagem);
+      return { ok: false };
+    } else {
+      localStorage.setItem('id', response?.data.id);
+      localStorage.setItem('token', response?.data.token);
+      this.setToken(response?.data.token);
+      this.setIdUser(response?.data.id);
+      this.setTypeUser('candidato');
+      return { ok: true };
+    }
   };
 }
